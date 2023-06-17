b27380268b20a8162d3519b97e2af267bc9d375b
Mehdi Khoudali <mehdikhoudalpro@gmail.com>
2023-06-17 22:51:26 +0200

    finished the landing page

--- eligius/app/page.tsx
+++ eligius/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from 'next/image'
-
-import Hero from "./components/landing/Hero"
-import NavBar from "./components/landing/NavBar"
-import Features from "./components/landing/Features"
+import Hero from "./sections/Hero";
+import NavBar from "./sections/NavBar";
+import Features from "./sections/Features";
+import Pricing from "./sections/Pricing";
 
 export default function Home() {
   return (
@@ -10,6 +9,7 @@ export default function Home() {
       <NavBar />
       <Hero />
       <Features />
+      <Pricing />
     </main>
   )
 }
--- eligius/app/sections/NavBar.tsx
+++ eligius/app/sections/NavBar.tsx
@@ -1,12 +1,9 @@
 export default function NavBar() {
-
     return (
-
         <nav className="bg-white dark:bg-gray-900 text-center items-center flex justify-between py-4 px-4 md:px-16">
-            <h3>EligiusAI</h3>
-            <a className="underline">Join the waitlist</a>
+            <h3>ELIGIUS.AI</h3>
+            <a href="#">Join the waitlist</a>
         </nav>
 
     )
-
 }
