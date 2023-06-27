ed35ca890c6e5222bf9ff23fd88a2390ac186458
Mehdi Khoudali <108628541+MehdiKhoudali@users.noreply.github.com>
2023-06-27 08:44:19 +0000

    new logo

--- eligius/app/sections/NavBar.tsx
+++ eligius/app/sections/NavBar.tsx
@@ -4,7 +4,7 @@ import { useState } from "react"
 
 import Image from "next/image"
 
-import Logo from '../assets/logo.png';
+import Logo from '../assets/eligius_logo.png';
 
 export default function NavBar() {
 
@@ -13,7 +13,7 @@ export default function NavBar() {
     return (
         <section className="w-full items-center top-0 left-0">
             <nav className="flex justify-between px-4 md:px-12 py-2 md:py-4 items-center">
-                <h2 className="text-[20px] text-gray-800 font-bold"><Image alt="Track the performances of your software team with leaderboards." className="w-[7rem] h-[4rem] md:h-[6rem] md:w-[11rem]" src={Logo} /></h2>
+                <h2 className="text-[20px] text-gray-800 font-bold"><Image alt="Track the performances of your software team with leaderboards." className="w-[120px]md:w-[175px] md:h-[65px]" src={Logo} /></h2>
                 <ul className='gap-4 md:gap-8 items-center hidden md:flex'>
                     <li><a href="#pricing" className="font-medium">Pricing</a></li>
                     <li><a href="#features" className="font-medium">Features</a></li>
