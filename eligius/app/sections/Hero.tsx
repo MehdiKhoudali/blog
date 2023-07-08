0fe58229f4635a121b716b1b1578ec4fc0636256
Mehdi Khoudali <108628541+MehdiKhoudali@users.noreply.github.com>
2023-07-08 17:27:53 +0000

    removed the gradients

--- eligius/app/sections/Hero.tsx
+++ eligius/app/sections/Hero.tsx
@@ -6,7 +6,7 @@ import { FaAngleRight, FaGithub } from "react-icons/fa";
 
 export default function Hero() {
     return (
-        <section className="md:min-h-[100vh] text-center px-4 bg-gradient-to-br from-transparent via-purple-300 to-transparent py-24 flex flex-col md:flex-row items-center justify-center gap-5 md:gap-8">
+        <section className="md:min-h-[100vh] text-center px-4 bg-white py-24 flex flex-col md:flex-row items-center justify-center gap-5 md:gap-8">
             <div className="h-full my-8 flex flex-col md:flex-row justify-center md:justify-between items-center w-full gap-8">
                 <div className="h-full md:w-[50%] flex text-center md:text-left flex-col gap-10 md:px-8">
                     <h1 className="sm:leading-[1.5rem] text-gray-800 font-bold text-[3rem] md:text-6xl">Your content creator's landing page in seconds</h1>
