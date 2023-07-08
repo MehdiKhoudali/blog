6aaaf3f28a8f360b418be27a3cb1ddc9614ca0cc
Mehdi Khoudali <108628541+MehdiKhoudali@users.noreply.github.com>
2023-07-08 17:16:28 +0000

    continue

--- eligius/app/sections/Cta.tsx
+++ eligius/app/sections/Cta.tsx
@@ -2,7 +2,7 @@ import JoinWaitlist from '../components/JoinWaitlist'
 
 export default function Cta() {
 	return (
-		<section className="h-[70vh] text-center px-4 bg-gradient-to-br from-transparent via-purple-300 to-transparent py-24 flex flex-col justify-center gap-5 md:gap-8">
+		<section className="md:h-[70vh] text-center px-4 bg-gradient-to-br from-transparent via-purple-300 to-transparent py-24 flex flex-col justify-center gap-5 md:gap-8">
             <h1 id="hero_h1" className="font-bold text-2xl text-center md:text-7xl text-gray-800">Publish your website<br />in seconds !</h1>      
             <div className="flex flex-col gap-2 md:gap-4 md:flex-row justify-center">
                 <JoinWaitlist />
